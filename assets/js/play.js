9240759a39b68c1d9e2a838427ba2fd73ba75d92
Dan-Swarts <Danstraws@gmail.com>
2024-09-08 11:35:10 -0500

    added a display to show if it's white's move or black's move

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,5 +1,6 @@
 boardArea = document.getElementById('board-area');
-homeBtn = document.getElementById('home-button')
+homeBtn = document.getElementById('home-button');
+playColorEl = document.getElementById('playColor');
 
 /* Timer functionality */
 const timerText = document.getElementById('timer');
@@ -12,6 +13,7 @@ let draggedTile = null;
 let tiles = [];
 let pieces = [];
 let highlightElement = null;
+let playColor = 'white';
 
 function startTimer() {
     let timer = timerTimeMinutes;
@@ -26,6 +28,16 @@ function startTimer() {
             display.innerText = "Game Finished!";
         }
     }, 1000);
+}
+
+function switchColor(){
+    if(playColor == 'white'){
+        playColor = 'black';
+        playColorEl.textContent = "Black's Move";
+    } else {
+        playColor = 'white';
+        playColorEl.textContent = "White's Move";
+    }
 }
 
 homeBtn.addEventListener('click',()=>{
@@ -51,17 +63,24 @@ boardArea.addEventListener('drop', (e) => {
 
     const position = pixelToIndex(e.clientX,e.clientY);
     const classList = draggedTile.classList
+    const destination = checkBoard(position.x,position.y,'piece');
 
-    if (classList.contains('piece')){
-
-        const destination = checkBoard(position.x,position.y,'piece');
+    if (classList.contains('fa-regular') && playColor == 'white'){
 
         if(destination != draggedTile){
             deletePiece(position.x, position.y);
         }
 
         placeObject(draggedTile, position.x, position.y);
+        switchColor();
 
+    } else if (classList.contains('fa-solid') && playColor == 'black'){
+
+        if(destination != draggedTile){
+            deletePiece(position.x, position.y);
+        }
+        placeObject(draggedTile, position.x, position.y);
+        switchColor();
     }
 
     draggedTile = null;
